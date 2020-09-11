bddcaed7a61940ec418a84fb65bb1d0670c12b62
pfig9141 <69915027+pfig9141@users.noreply.github.com>
2020-09-11 11:14:25 +0200

    Add files via upload

---
+++ dsp_fcyfrowy_wektory.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### przykład filtracji cyfrowej pewnego sygnału
+
+# In[7]:
+
+
+import numpy as np
+from scipy import signal
+import matplotlib.pyplot as py
+a = np.arange(0,10000,1)
+x = signal.chirp(a,0.001,9999,0.003) + np.random.randn(10000)
+b = signal.firwin(1288,0.1)
+y = signal.lfilter(b,1,x)
+fig, ax = py.subplots(figsize=(10,10))
+ax1 = py.subplot(211)
+py.plot(a,x)
+ax2 = py.subplot(212)
+py.plot(a,y)
+print(a.shape)
+print(x.shape)
+print(b.shape)
+print(y.shape)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+

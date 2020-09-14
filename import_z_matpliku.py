984cb46bcd53c60c9bce2657486582c3b2450a66
pfig9141 <69915027+pfig9141@users.noreply.github.com>
2020-09-14 09:56:35 +0200

    Add files via upload

---
+++ import_z_matpliku.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  1 09:31:25 2020
+
+@author: figonpiot
+"""
+
+import numpy as np
+import scipy.io 
+mat = scipy.io.loadmat("G:\czerwony komputer\GIHA\Rejestracje\R1uF_mat");
+matlist = list(mat.values());
+matdata = matv[3];

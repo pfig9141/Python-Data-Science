9b8bf4bdc9004b6f98f93ab2ec7ad8d6907e2fea
_Qlopez <69915027+pfig9141@users.noreply.github.com>
2020-09-25 17:16:55 +0200

    Create dsp_dft_macierzGen.py

---
+++ dsp_dft_macierzGen.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+N = 256
+n = np.arange(256)
+I = np.array( [ np.exp(-2*np.pi/N*k*n*1j) for k in range(256) ] )
+f = 2
+plt.plot(np.arange(N),I[f,:].real,'r',np.arange(N),I[f,:].imag,'k')
+f = 254
+plt.plot(np.arange(N),I[f,:].real,'g',np.arange(N),I[f,:].imag,'b')
